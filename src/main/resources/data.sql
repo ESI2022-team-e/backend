b1a206f080af61dcebf3ba2e38a43449e3d528c6
manapa <manapa@ttu.ee>
2022-04-30 21:37:16 +0300

    Postman tests added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ TRUNCATE cars,reservations RESTART IDENTITY;
 
 INSERT INTO cars (id,mark, model, nr_of_seats, transmission_type, fuel_type, daily_cost, year, licence_plate) VALUES
     ('a81bc81b-dead-4e5d-abff-90865d1e13b1','Ford','Focus 1.6 Aut',5,'Automatic','Petrol',71,2002,'123 ABC'),
-    ('a81bc81b-dead-4e5d-abcf-90865d1e98r2','Mercedes-Benz','C 220 AMG',5,'Automatic','Diesel',50,2012,'456 DEF');
+    ('c8203520-c8b0-11ec-9d64-0242ac120002','Mercedes-Benz','C 220 AMG',5,'Automatic','Diesel',50,2012,'456 DEF');
