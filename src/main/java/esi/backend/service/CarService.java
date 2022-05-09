07c6bcace44d801129cb4b75410904b806c4a5d7
mnapa <94014946+mnapa@users.noreply.github.com>
2022-05-09 15:57:36 +0300

    @RequestParams added to getAllCars method (#11)
    * @RequestParams added to getAllCars method
    
    * ResponseEntities added to CarService and CarController
    
    * More ResponseEntities added to CarService and CarController
    
    * Initial logic for getting only the available cars added
    
    * RequestParams set as optional, query does not work yet
    
    * Query works

--- src/main/java/esi/backend/service/CarService.java
+++ src/main/java/esi/backend/service/CarService.java
@@ -1,13 +1,18 @@
 package esi.backend.service;
 
 import esi.backend.model.Car;
+import esi.backend.model.Request;
+import esi.backend.model.RequestStatus;
 import esi.backend.repository.CarRepository;
+import esi.backend.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -15,36 +20,58 @@ public class CarService {
 
     @Autowired
     private CarRepository carRepository;
+    @Autowired
+    private RequestRepository requestRepository;
 
-    public List<Car> getAllCars() {
+    public ResponseEntity<List<Car>> getAllCars(LocalDateTime startTime, LocalDateTime endTime) {
         List<Car> cars = new ArrayList<>();
         carRepository.findAll().forEach(cars::add);
-        return cars;
-    }
 
-    public Optional<Car> getCar(UUID id) {
-        return carRepository.findById(id);
-    }
-
-    public void addCar(Car car) {
-        carRepository.save(car);
-    }
-
-    public void updateCar(Car car, UUID carId) {
-        Optional<Car> carInService = carRepository.findById(carId);
-
-        if (carInService.isPresent())
-            if (car.getDaily_cost() >= 0) {
-                carInService.get().setDaily_cost(car.getDaily_cost());
+        if (startTime != null && endTime != null) {
+            List<Request> requests = new ArrayList<>(requestRepository.findAll());
+            if (!requests.isEmpty()) {
+                for (Request r : requests) {
+                    if (r.getPickup_datetime().isBefore(endTime) && startTime.isBefore(r.getDropoff_datetime())) {
+                        if (r.getStatus() == RequestStatus.ACCEPTED || r.getStatus() == RequestStatus.PENDING) {
+                            cars.remove(r.getCar());
+                        }
+                    }
+                }
             }
-        if (car.getLicence_plate() != null) {
-            carInService.get().setLicence_plate(car.getLicence_plate());
         }
-        carRepository.save(carInService.get());
+        return new ResponseEntity<>(cars, HttpStatus.OK);
     }
 
-    public void deleteCar(UUID id) {
+    public ResponseEntity<Car> getCar(UUID id) {
+        Car car = carRepository.findById(id).orElse(null);
+        return (car == null)
+                ? new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                : new ResponseEntity<>(car, HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> addCar(Car car) {
+        carRepository.save(car);
+        return ResponseEntity.ok("Car added successfully!");
+    }
+
+    public ResponseEntity<?> updateCar(Car newCar, UUID carId) {
+        Car car = carRepository.findById(carId).orElse(null);
+        if (car == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if (newCar.getDaily_cost() >= 0) {
+            car.setDaily_cost(newCar.getDaily_cost());
+        }
+        if (newCar.getLicence_plate() != null) {
+            car.setLicence_plate(newCar.getLicence_plate());
+        }
+        carRepository.save(car);
+        return ResponseEntity.ok("Car updated successfully!");
+    }
+
+    public ResponseEntity<?> deleteCar(UUID id) {
         carRepository.deleteById(id);
+        return ResponseEntity.ok("Car deleted successfully!");
     }
 }
 
