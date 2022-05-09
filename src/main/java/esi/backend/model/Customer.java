6658cc4203b0b0b13587d732d9f488bca860f6e2
manapa <manapa@ttu.ee>
2022-05-09 20:37:18 +0300

    Getters and setters added for the added attributes in the Customer class

--- src/main/java/esi/backend/model/Customer.java
+++ src/main/java/esi/backend/model/Customer.java
@@ -31,6 +31,54 @@ public class Customer extends User {
     private Set<Invoice> invoices = new HashSet<>();
 
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getDriversLicenseNumber() {
+        return driversLicenseNumber;
+    }
+
+    public void setDriversLicenseNumber(String driversLicenseNumber) {
+        this.driversLicenseNumber = driversLicenseNumber;
+    }
+
+    public String getWorkingPermitNumber() {
+        return workingPermitNumber;
+    }
+
+    public void setWorkingPermitNumber(String workingPermitNumber) {
+        this.workingPermitNumber = workingPermitNumber;
+    }
+
+    public String getIdentificationDocumentNumber() {
+        return identificationDocumentNumber;
+    }
+
+    public void setIdentificationDocumentNumber(String identificationDocumentNumber) {
+        this.identificationDocumentNumber = identificationDocumentNumber;
+    }
+
     public Set<Rental> getRentals() {
         return rentals;
     }
