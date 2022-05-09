0befdd76d1827db4e166e0e86f33e21972eaca51
manapa <manapa@ttu.ee>
2022-05-09 20:35:07 +0300

    Add missing attributes to the Customer class

--- src/main/java/esi/backend/model/Customer.java
+++ src/main/java/esi/backend/model/Customer.java
@@ -3,12 +3,21 @@ package esi.backend.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @DiscriminatorValue("user")
 public class Customer extends User {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate dateOfBirth;
+    private String driversLicenseNumber;
+    private String workingPermitNumber;
+    private String identificationDocumentNumber;
+
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "customer", cascade = CascadeType.ALL)
     @JsonIgnore
     private Set<Rental> rentals = new HashSet<>();
